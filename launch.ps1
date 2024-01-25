6ab399c840db3203a885e8aafbadd9d78a824325
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-01-25 15:02:39 -0700

    Create launch.ps1

---
+++ launch.ps1
@@ -0,0 +1,8 @@
+<# Start Attack App #>
+$attackApp = Get-Process ironcat_attack
+if ( -not $attackApp ) {
+    Start-Process -FilePath "C:\Program Files\psattack\ironcat_attack.exe" -WorkingDirectory "C:\Program Files\psattack\" -WindowStyle Hidden
+}
+
+<# Start Firefox #>
+Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -ArgumentList "-new-window localhost:28657/endpoint.html" -WorkingDirectory "C:\Program Files\Mozilla Firefox\" -WindowStyle Hidden 
